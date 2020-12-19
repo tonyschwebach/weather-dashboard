2db36ccd544035b877d9f0ab7accffa2e9b99835
T Schway <tony.schwebach@gmail.com>
2020-12-19 16:44:57 -0500

    added functionality for search button

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 $(document).ready(function () {
   console.log("hello world");
 
+  // DOM VARIABLES
+  var searchBoxEl = $("#search-box");
+  var searchButtonEl = $("#search-button");
+  var searchHistoryEl = $("#search-history");
+
+  var currentWeatherEl = $("#current-weather");
+  var forecastEl = $("#five-day");
+
+  // JAVASCRIPT VARIABLES
+  var searchedCities = ["Philadelphia"];
+  var apiKey = ""
+  var currentCity = "Atlanta";
+
+  // FUNCTION DEFINITIONS
+  // initialize search history from local storage
+
+  // add new city to search history and local storage
+  function storeHistory(newCity){
+    searchedCities.unshift(newCity);
+  }
+  // ajax GET current
+  function currentWeather(){
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q="+currentCity+"&appid="+apiKey;
+    console.log(queryURL);
+
+  }
+  // ajax UV index
+  // ajax GET 5 day
+  // render search history
+  // render current day
+  // render forecast
+
+  // FUNCTION CALLS
+
+  // EVENT LISTENERS
+  // on search click
+  $("#search-button").on("click", function(event) {
+    event.preventDefault();
+    newCity = searchBoxEl.val();
+    storeHistory(newCity);
+    currentCity = newCity
+    currentWeather();
+ 
+  });
+  // on search history click with event delegation
 
   // GIVEN a weather dashboard with form inputs
   // WHEN I search for a city
@@ -15,16 +60,4 @@ $(document).ready(function () {
   // THEN I am again presented with current and future conditions for that city
   // WHEN I open the weather dashboard
   // THEN I am presented with the last searched city forecast
-
-
-
-
-
-
-
-
-
-
-
-
 });
